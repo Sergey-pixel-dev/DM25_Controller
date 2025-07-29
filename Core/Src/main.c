ddb69b9543cc74d0ec5962bc1fd50436643e8ddc
Sergey-pixel-dev <sonin.serej@yandex.ru>
2025-07-29 13:46:09 +0300

    добавил подтяжку

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -625,7 +625,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : PB12 PB13 PB14 PB15 */
   GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_3 | GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
